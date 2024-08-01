1474df6152199f70aa2cc2c17fbae4982d6dce3f
YuraN <yuranazarkevych767@gmail.com>
2024-08-01 21:56:45 +0300

    Fixed bugs and added popups

--- web.py
+++ web.py
@@ -5,9 +5,17 @@ todos = functions.get_todos()
 
 
 def add_todo():
-    todo = st.session_state["new_todo"] + '\n'
-    todos.append(todo)
-    functions.write_todos(todos)
+    todo = st.session_state["new_todo"].strip()
+    if todo and todo not in [t.strip() for t in todos]:
+        todo += "\n"
+        todos.append(todo)
+        functions.write_todos(todos)
+        st.toast(f"Todo '{todo.strip()}' added!", icon="✅")
+    elif todo in [t.strip() for t in todos]:
+        st.toast(f"Todo '{todo.strip()}' already exist!", icon="⚠️")
+    else:
+        st.error("Todo cannot be empty!")
+    st.session_state["new_todo"] = ""
 
 
 st.title("My Todo App")
@@ -22,5 +30,5 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()
 
-st.text_input(label="", placeholder="Add new todo...",
+st.text_input(label="Enter todo", label_visibility="hidden", placeholder="Add new todo...",
               on_change=add_todo, key="new_todo")
